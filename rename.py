3c52c5416ac61ca0c76ab39820c1d868074a92d4
chenyi <chenyi852@gmail.com>
2017-02-06 23:24:47 +0100

    use absolute path

--- rename.py
+++ rename.py
@@ -9,9 +9,17 @@ import shutil
 
 path="/home/chenyi/音乐"
 
-list_file = os.listdir(path)
-for filename in list_file :
-    #将文件名中"[mqms2](1)"去掉
-    newname=filename.replace("[mqms2](1)", "")
-    newname=newname.replace("[mqms2]", "")
-    shutil.move(filename, newname)
+def modify_name(file_path):
+	list_file = os.listdir(file_path)
+	for item in list_file :
+		#将文件名中"[mqms2](1)"去掉
+		filename = filepath + '\\' + item
+		newname=filename.replace("[mqms2](1)", "")
+		newname=newname.replace("[mqms2]", "")
+		if not (os.path.normcase(filename) == os.path.normcase(newname)):
+			print "move " + filename + "to " + newname
+			shutil.move(filename, newname)
+
+if __name__ == '__main__':
+	modify_name(r'D:\03_personal\01_music')
+	
