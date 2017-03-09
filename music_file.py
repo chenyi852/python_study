eca9c20cc11686a9028bf9c7f5aad7d0494745b0
chenyi <chenyi852@gmail.com>
2017-03-09 19:57:41 +0100

    simplify the rm affix

--- music_file.py
+++ music_file.py
@@ -1,16 +1,11 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
+#!/usr/bin/env python
+#coding=utf-8
 
 import os
-import sys
-import re
 import shutil
 import filecmp
-
-download_path=r'C:\Users\c00200500.CHINA\Downloads'
-music_path=r'D:\03_personal\01_music'
-path="/home/chenyi/音乐"
+dl_path=r'E:\BaiduYunDownload'
+msc_path=r'E:\music'
 
 def endwith(*endstring):
 	ends = endstring
@@ -18,34 +13,29 @@ def endwith(*endstring):
 		f = map(s.endswith,ends)
 		if True in f: return s
 	return run
-  
-def rm_suffix(srcpath):
-	list_file = os.listdir(srcpath)
-	for item in list_file :
-		#将文件名中"[mqms2](1)"去掉
-		filename = srcpath + '\\' + item
-		newname=filename.replace("[mqms2](1)", "")
-		newname=newname.replace("[mqms2]", "")
-		if not (os.path.exists(newname)):
-			print "move " + filename + " to " + newname
-			shutil.move(filename, newname)
 
+def rm_suffix(item):
+	file=item.replace("[mqms2](1)", "")
+	new=file.replace("[mqms2]", "")
+	nfile=new.replace(' ', '')
+	return nfile
+	
 def cp_download(src_path, dst_path):
-	left_files=filecmp.dircmp(src_path,dst_path).left_only
-	a = endwith('.mp3','.flac','.m4a', '.ape')
-	f_files = filter(a, left_files)
+	files=filecmp.dircmp(src_path, dst_path).left_only
+	a=endwith('.mp3','.flac', 'm4a', 'ape')
+	f_files=filter(a, files)
+	
 	for item in f_files:
-		src_file = src_path + '\\' + item
-		dst_file = dst_path + '\\' + item
-		if not (os.path.isdir(src_file)):
-			# check if the file is exist, before move another file to this file.
-			if not (os.path.exists(dst_file)):
-				print "move {src} to {dst}" .format(src=src_file, dst=dst_file)
-				shutil.move(src_file, dst_file)
-				
-
-			
+		file=rm_suffix(item)
+		src_file=src_path + '\\' + item
+		dst_file=dst_path + '\\' + file
+		if  not ((os.path.isdir(src_file)) or (os.path.exists(dst_file))):
+			print "copy " + src_file +" to " + dst_file
+			shutil.move(src_file, dst_file)
+		
+	
+	
+	
 if __name__ == "__main__":
-	print "copy {src} to {dst}".format(src=download_path, dst=music_path)
-	cp_download(download_path, music_path)
-	rm_suffix(music_path)
+	cp_download(dl_path, msc_path)
+	
