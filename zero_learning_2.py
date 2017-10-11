cdb75961e41b874d51a473417eb7f9a946e5681a
chenyi 00200500 <johnny.chenyi@huawei.com>
2017-10-11 23:19:36 +0200

    add zero learning demo

---
+++ zero_learning_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+print id(3)
+print type(3)
+print 2+3
+
+"""
+please caculate 19+2*4-8/2
+"""
+a=19+2*4-8/2
+print a
+print 'what\'s your name?'
+print 'py'+'thon'
+
