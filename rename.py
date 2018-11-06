af3910112fbe8368c5d7b89a0fddc730fb9c0843
chenyi 00200500 <johnny.chenyi@huawei.com>
2018-11-07 00:30:51 +0800

    use regular express to match the file name

--- rename.py
+++ rename.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 
@@ -8,19 +8,46 @@ import re
 import shutil
 
 path="/home/chenyi/音乐"
+windows_path="D:\03_personal\01_music"
+cygwin_path="/cygdrive/d/03_personal/01_music"
 
 def modify_name(file_path):
-	list_file = os.listdir(file_path)
-	for item in list_file :
-		#将文件名中"[mqms2](1)"去掉
-		filename = file_path + '\\' + item
-		newname=filename.replace("[mqms2](1)", "")
-		newname=newname.replace("[mqms2]", "")
-		newname=newname.replace(' ', '')
-		if not (os.path.normcase(filename) == os.path.normcase(newname)):
-			print ("move " + filename + "to " + newname)
-			shutil.move(filename, newname)
+    list_file = os.listdir(file_path)
+    for item in list_file :
+    #将文件名中"[mqms2](1)"去掉
+        filename=file_path + '/' + item
+
+        newname=re.sub(r'\[\w*\]\(*\d*\)*', "", filename)
+        print("%s --> %s" %(filename, newname))
+        cleanup_filename(filename)
+        """
+	    ewname=filename.replace("[mqms2](1)", '')
+	    newname=newname.replace("[mqms2]", '')
+	    newname=newname.replace("[mqms]", '')
+	    newname=newname.replace(' ', '')
+	"""
+        if not (os.path.normcase(filename) == os.path.normcase(newname)):
+            print ("move " + filename + "to " + newname)
+            shutil.move(filename, newname)
+
+def cleanup_filename(myfile):
+    new = re.sub(r'\[\w*\]\(*\d*\)*', "", myfile)
+    print("file is %s, new file name is %s" %(myfile, new))
 
 if __name__ == '__main__':
-	modify_name(r'D:\03_personal\01_music')
-	
+    if os.path.exists(path):
+        print("%s exists" %(path))
+        modify_name(path)
+    if os.path.exists(windows_path):
+        print("%s exists" %(windows_path))
+        modify_name(windows_path)
+    if os.path.exists(cygwin_path):
+        print("%s exists" %(cygwin_path))
+        modify_name(cygwin_path)
+    cleanup_filename("my[mqms2].mp3")
+    """ 
+        for test
+	cleanup_filename("my[mqms2].mp3")
+	cleanup_filename("my[mqms2](1).mp3")
+	cleanup_filename("my[mqms].mp3")
+    """
